6d5dd24c41c493d4a0a1f3b33cf93396dd449316
Nikita <43483640+Si-ja@users.noreply.github.com>
2020-02-19 00:33:55 +0100

    Updating the README file and adding of support functions

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 This is a simple application that is built using python. The essence of it is that it can analyse the sentiment of an input sentence that is given to it in English.
 
 ## Current Status
-__Last Update:__ 2020/02/18
+__Last Update:__ 2020/02/19
 
 Project Initiated | Support Function Made | App Skeleton Created | The Full App Prepared | App Deployable
 :------------: |:------------: |:------------: |:------------: |:------------: |
-:heavy_check_mark: | :x: | :x: | :x: | :x:
+:heavy_check_mark: | :heavy_check_mar: | :x: | :x: | :x:
 
 ## Packages Used
 The Application utilizes several packages to operate:
@@ -21,7 +21,7 @@ Create an application using python with its own GUI that can analyse sentiment o
 
 - [x] Create a repository
 - [x] Create a README file
-- [ ] Create a support script that will install all needed packages
+- [x] Create a support script that will check if  all the needed packages are ready
 - [ ] Create a .kv file with the app skeleton
 - [ ] Add information section in the app
 - [ ] Add settings section in the app
--- Support Functions/dependencies.py
+++ Support Functions/dependencies.py
@@ -1,44 +1,26 @@
-# ============================
-# %%%% PREPARE ARGUMENTS %%%%%
-# ============================
-import argparse
-from argparse import RawTextHelpFormatter
-parser = argparse.ArgumentParser(description = "\t\t\t\tDescription:\n"
-                                "--------------------------------------------------------------------------\n"
-                                "Use this file to prepare your environment if it is missing certain packages"
-                                "in an automated way."
-                                "--------------------------------------------------------------------------"
-                                formatter_class = RawTextHelpFormatter)
+# ========================
+# %%%% MAIN FUNCTION %%%%%
+# ========================
+def prepare_packages():
+    try:
+        import nltk
+        print(">You have an nltk package install under {} version.".format(nltk.__version__))
+        if nltk.__version__ != "3.4.5":
+            print("\t>>It is recommended to update to the 3.4.5 version.")
+    except:
+        print("You are missing an nltk package.")
 
-parser.add_arugment("-i", "--install",
-                    type = str,
-                    metavar = "",
-                    help = "Indicate [y] or [yes] if you want to have all needed packages installed in your"
-                           "environment for the project to work.")
-
-parser.add_argument("-c", "--check",
-                    type = str,
-                    metavar = "",
-                    help = "Indicate [y] or [yes] if you would like to check whether you have needed packages"
-                           "for the project to work on your side.")
-args = parser.parse_args()
-
-# ============================
-# %%%% PREPARE FUNCTIONS %%%%%
-# ============================
-
-def prepare_packages(install = None, check = None):
-    """
-
-    :param install: Decides if there is a need to install missing packages
-    :param check:  Checks if all the needed packages are present to work with the project
-    :return: void
-    """
-    pass
+    try:
+        import kivy
+        print(">You have a kivy package install under {} version.".format(kivy.__version__))
+        if kivy.__version__ != "1.11.1":
+            print("\t>>It is recommended to update to the 1.11.1 version.")
+    except:
+        print("You are missing a kivy package.")
 
 # ======================
 # %%%% IF __NAME__ %%%%%
 # ======================
 
 if __name__ == "__main__":
-    prepare_packages(args.install, args.check)
+    prepare_packages()
