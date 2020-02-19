4d793235ff6f037659827006c22ba37560a0982f
Si-ja <nicksemion@gmail.com>
2020-02-19 16:52:28 +0100

    Main files update

--- src/MainApp.py
+++ src/MainApp.py
@@ -6,6 +6,14 @@ from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.checkbox import CheckBox
+
+# Conditions for the settings menu
+global prediction_set
+global percentage_set
+global input_set
+prediction_set, percentage_set, input_set = True, False, True
+
 
 class MainWindow(Screen):
     """Main Window of the App"""
@@ -17,7 +25,32 @@ class InformationWindow(Screen):
 
 class SettingsWindow(Screen):
     """Possible Settings a user can toggle"""
-    pass
+    def checkbox_click_prediction(self, instance, value):
+        if value is True:
+            prediction_set = True
+        else:
+            prediction_set = False
+            
+    def checkbox_click_prediction_get(self, instance):
+        return prediction_set
+    
+    def checkbox_click_percentage(self, instance, value):
+        if value is True:
+            percentage_set = True
+        else:
+            percentage_set = False
+            
+    def checkbox_click_percentage_get(self, instance):
+        return percentage_set
+            
+    def checkbox_click_input(self, instance, value):
+        if value is True:
+            input_set = True 
+        else:
+            input_set = False
+            
+    def checkbox_click_input_get(self, instance):
+        return input_set
 
 class AnalysisWindow(Screen):
     """Actual sentence Analysis window"""
