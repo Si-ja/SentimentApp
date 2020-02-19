ff5c99a1b757dca3e26b1e2188f1cc5435cca9c7
Si-ja <nicksemion@gmail.com>
2020-02-19 10:22:05 +0100

    Logical update the the Dependencies script

--- Support Functions/dependencies.py
+++ Support Functions/dependencies.py
@@ -6,7 +6,7 @@ def prepare_packages():
         import nltk
         print(">You have an nltk package install under {} version.".format(nltk.__version__))
         if nltk.__version__ != "3.4.5":
-            print("\t>>It is recommended to update to the 3.4.5 version.")
+            print("\t>>It is recommended to switch to the 3.4.5 version, as under it the original build took place.")
     except:
         print("You are missing an nltk package.")
 
@@ -14,7 +14,7 @@ def prepare_packages():
         import kivy
         print(">You have a kivy package install under {} version.".format(kivy.__version__))
         if kivy.__version__ != "1.11.1":
-            print("\t>>It is recommended to update to the 1.11.1 version.")
+            print("\t>>It is recommended to switch to the 1.11.1 version, as under it the original build took place.")
     except:
         print("You are missing a kivy package.")
 
