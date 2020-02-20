4cd52eb4c2d88a7b7f34e2999f45ef14ca05a095
Si-ja <nicksemion@gmail.com>
2020-02-20 12:31:00 +0100

    TODO: Sentiment analysis

--- src/MainApp.py
+++ src/MainApp.py
@@ -9,18 +9,24 @@ from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
 
 # Conditions for the settings menu (not bad, not terrible)
-global prediction_set
+global sentence_set
 global percentage_set
 global input_set
 global data
+
+global sentence_get
+sentence_get = ""
 
 # Read the json file with settings and decide how the application needs to start
 json_data = "settings.json"
 with open('settings.json') as json_file:
     data = json.load(json_file)
-prediction_set = data["prediction"]
+sentence_set = data["sentence"]
 percentage_set = data["percentage"]
 input_set = data["input"]
 
@@ -32,29 +38,41 @@ def save_json(data = data):
     with open('settings.json', 'w') as json_file:
         json.dump(data, json_file)
 
-
 class MainWindow(Screen):
     """Main Window of the App"""
-    pass
-
+    def get_sentence(self):
+        """Get the sentence the user wants to analyse"""
+        data["text"] = self.ids.sentence.text
+        save_json()
+        
+        # Also check if the input window needs to be cleared
+        if data["sentence"]:
+            label = self.ids.sentence
+            label.text = ""
+            
+        # Here analysis needs to be performed
+        pass
+            
+        
+          
 class InformationWindow(Screen):
     """Some information on the App"""
     pass
 
 class SettingsWindow(Screen):
     """Possible Settings a user can toggle"""    
-    def checkbox_click_prediction(self, instance, value):
+    def checkbox_click_sentence(self, instance, value):
         if value is True:
-            prediction_set = True
-            data["prediction"] = True
+            sentence_set = True
+            data["sentence"] = True
             save_json()
         else:
-            prediction_set = False
-            data["prediction"] = False
+            sentence_set = False
+            data["sentence"] = False
             save_json()
     
-    def checkbox_click_prediction_get(self, instance):
-        return prediction_set
+    def checkbox_click_sentence_get(self, instance):
+        return sentence_set
     
     def checkbox_click_percentage(self, instance, value):
         if value is True:
@@ -84,11 +102,39 @@ class SettingsWindow(Screen):
 
 class AnalysisWindow(Screen):
     """Actual sentence Analysis window"""
+    def sentence_analysis(self):
+        pass
+        #Get the sentence:
+        #sentence = self.root.ids.sentence.text
+        #print(sentence)
+
     def verify(self):
+        # Check the conditions user has set to work with by loading them
         with open('settings.json') as json_file:
             data = json.load(json_file)
-        prediction_set = data["prediction"]
-        return prediction_set
+    
+        # Check if the sentence needs to be entered:
+            if data["input"]:
+                label = self.ids.my_sentence
+                label.text = "Sentence: {}".format(str(data["text"]))
+            else:
+                label = self.ids.my_sentence
+                label.text = ""
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+        percentage_set = data["sentence"]
+        
+        
+        
+        return
 
 class WindowManager(ScreenManager):
     """App windows' manager"""
